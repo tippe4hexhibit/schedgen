068ded6b92b29ec30e256d53f549da8328f808b9
Corey S. Seliger <corey@seligerfamily.org>
2023-06-24 15:22:04 -0400

    Output Files
    - Leverage environment variables for inputs (makes it easy to use with GitHub Actions)
    - Output to files based on an existing output directory (env var)

--- schedgen/application.py
+++ schedgen/application.py
@@ -1,6 +1,8 @@
 import logging
 import os
 
+from pathlib import Path
+
 from pyairtable import Api
 from schedgen.models.schedule import FullSchedule
 from schedgen.models.yaml import YamlSchedule
@@ -12,18 +14,51 @@ class SchedGenApp:
 
     @staticmethod
     def run():
+
+        required_env_vars = [
+            'AIRTABLE_API_KEY',
+            'AIRTABLE_BASE',
+            'AIRTABLE_TABLE',
+            'OUTPUT_DIR'
+        ]
+
+        check_env_vars = [x in os.environ for x in required_env_vars]
+
+        if not all(check_env_vars):
+            log.error(f'{required_env_vars}: {check_env_vars}')
+            log.error('Ensure all environment variables are set properly')
+            exit(255)
+        else:
+            for env_var in required_env_vars:
+                if 'KEY' in env_var:
+                    log.info(f'{env_var} is set')
+                else:
+                    log.info(f'{env_var} is {os.environ[env_var]}')
+
+        output_path = Path(os.environ['OUTPUT_DIR'])
+
+        if not output_path.exists():
+            log.error(f"OUTPUT_DIR ({os.environ['OUTPUT_DIR']}) does not exist.")
+            exit(254)
+
         api = Api(os.environ['AIRTABLE_API_KEY'])
 
-        table = api.get_table('appywWGDJU508OseV', 'tblJHQ4V4VfCdVlFg')
+        table = api.get_table(os.environ['AIRTABLE_BASE'], os.environ['AIRTABLE_TABLE'])
         raw_schedule = table.all(sort=['Event Start'])
 
+        # Build the object structure from the Airtable data
         full_schedule = FullSchedule(raw_schedule)
 
-        # for schedule in full_schedule.schedules:
-        #     log.info(full_schedule.schedules[schedule])
+        # Dump out all the YAMLs for each schedule type, by date
+        schedule_prefix = None
+        for schedule_type, schedule in full_schedule.get_schedules().items():
+            schedule_yamls = YamlSchedule(schedule)
+            schedule_prefix = schedule.schedule_abbreviation + '_'
 
-        fair_schedule_yamls = YamlSchedule(full_schedule.get_schedules()['Full Schedule'])
+            for yaml_date, yaml in schedule_yamls.get_yamls().items():
+                schedule_filename = schedule_prefix + ''.join(yaml_date.split('-')) + '.yaml'
 
-        for yaml_date, yaml in fair_schedule_yamls.get_yamls().items():
-            print(yaml_date)
-            print(yaml)
+                with open(output_path / schedule_filename, 'w') as yaml_out:
+                    log.info(f'Writing {schedule_type} schedule file for {yaml_date} called {schedule_filename}')
+                    yaml_out.write(yaml)
+
