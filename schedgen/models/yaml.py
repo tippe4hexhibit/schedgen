89d43768b8f5acc133c1faa06d3f1ae8d49961cc
Corey S. Seliger <corey@seligerfamily.org>
2023-07-11 21:30:37 -0400

    Restructure YAML
    - Worked around an issue with Hugo that it unintentionally sorts its dictionaries which caused time to appear out of order in certain cases.

--- schedgen/models/yaml.py
+++ schedgen/models/yaml.py
@@ -1,6 +1,5 @@
 import logging
 
-from collections import OrderedDict
 from datetime import datetime
 
 from yaml import load, dump
@@ -17,22 +16,25 @@ class YamlSchedule:
     def __init__(self, event_schedule):
         self.yamls = {}
 
-
-
         for event_date, schedule in event_schedule.get_events().items():
+            time_lookup = {}
             yaml_struct = {
                 'longdate': datetime.fromisoformat(event_date).strftime('%A, %B %-d, %Y'),
-                'times': {}
+                'times': []
             }
 
             for event_start_time, venue in schedule.items():
                 for venue_name, events in venue.items():
                     for event in events:
-                        if event_start_time not in yaml_struct['times']:
-                            yaml_struct['times'][event_start_time] = {}
+                        if event_start_time not in time_lookup:
+                            time_lookup[event_start_time] = len(yaml_struct['times'])
+                            yaml_struct['times'].append({
+                                'time': event_start_time,
+                                'venues': {}
+                            })
 
-                        if venue_name not in yaml_struct['times'][event_start_time]:
-                            yaml_struct['times'][event_start_time][venue_name] = []
+                        if venue_name not in yaml_struct['times'][time_lookup[event_start_time]]:
+                            yaml_struct['times'][time_lookup[event_start_time]]['venues'][venue_name] = []
 
                         event_dict = {
                             'event_name': event['event_name'].strip()
@@ -44,7 +46,7 @@ class YamlSchedule:
                         if 'event_end_time' in event.keys():
                             event_dict['event_end_time'] = event['event_end_time'].strip().replace('\n', ' ')
 
-                        yaml_struct['times'][event_start_time][venue_name].append(event_dict)
+                        yaml_struct['times'][time_lookup[event_start_time]]['venues'][venue_name].append(event_dict)
 
             self.yamls[event_date] = dump(yaml_struct, Dumper=Dumper, sort_keys=False)
 
